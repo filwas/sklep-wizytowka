292c2147b708842580ca86af47382f6df99abb89
fwas <fwas.corp@gmail.com>
2024-05-22 17:29:51 +0200

    parallax image tweaking

--- app/ui/pageSegments/ParallaxImage.tsx
+++ app/ui/pageSegments/ParallaxImage.tsx
@@ -5,8 +5,12 @@ import styles from "./ParallaxImage.module.css";
 import { ParallaxBanner, ParallaxBannerLayer } from "react-scroll-parallax";
 import { useCloudinary } from "@/app/providers";
 import { AdvancedImage } from "@cloudinary/react";
-
-import { name } from "@cloudinary/url-gen/actions/namedTransformation";
+import { crop } from "@cloudinary/url-gen/actions/resize";
+import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
+import { format, quality } from "@cloudinary/url-gen/actions/delivery";
+import { auto as autoQuality } from "@cloudinary/url-gen/qualifiers/quality";
+import { auto as autoFormat } from "@cloudinary/url-gen/qualifiers/format";
+import useGetScreenWidth from "@/utils/useGetScreenWidth";
 
 interface ParallaxImageProps {
   foto: CloudinaryResource;
@@ -16,11 +20,18 @@ export default function ParallaxImage(props: ParallaxImageProps) {
   const cld = useCloudinary();
   const image = cld
     .image(props.foto.public_id)
-    .namedTransformation(name("createParallaxImage"));
+    .resize(
+      crop()
+        .height(useGetScreenWidth() + 2000)
+        .aspectRatio("9:16")
+        .gravity(autoGravity())
+    )
+    .delivery(quality(autoQuality()))
+    .delivery(format(autoFormat()));
 
   return (
     <div className={styles.parallaxWrapper} id="">
-      <ParallaxBanner style={{ aspectRatio: "2/1" }}>
+      <ParallaxBanner style={{ aspectRatio: "1.8/1" }}>
         <ParallaxBannerLayer style={{ zIndex: "1000" }}>
           <div className={styles.parallaxText}>
             <h1>OGRODOWY CHILLOUT</h1>
